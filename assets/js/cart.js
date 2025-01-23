61851725fd387f70e80e0b7081fb2bb3bf17c8a0
SkylineFX <rrstudor@gmail.com>
2025-01-23 20:53:54 +0200

    final

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -34,7 +34,7 @@ async function addToCart(currentProductId, newProduct = true) {
 
 async function fetchProduct(currentProductId) {
   const response = await fetch('./assets/products.json');
-  const json = response.json();
+  const json = await response.json();
   return json.product.find(product => product.id == currentProductId);
 }
 
